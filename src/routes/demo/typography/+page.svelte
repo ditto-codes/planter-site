2459d5fc143c7f75a42adb204bae7f60ec1326bd
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2023-02-22 21:44:13 -0500

    updated typography

--- src/routes/demo/typography/+page.svelte
+++ src/routes/demo/typography/+page.svelte
@@ -7,6 +7,9 @@
 <Section 
   title="Headings" 
 >
+  <Example label="DISPLAY">
+    <h1 class="display">Add to your library.</h1>
+  </Example>
   <Example label="H1">
     <h1 as="h1">The five boxing wizards jump quickly.</h1>
   </Example>
@@ -34,7 +37,7 @@
     <p class="lead">Next they turned to the business table. </p>
   </Example>
   <Example label="P" top>
-    <p>Next they turned to the business table. On the desk, among the neat array of papers, a large envelope was uppermost, and bore, in the doctor’s hand, the name of Mr. Utterson. The lawyer unsealed it, and several enclosures fell to the floor. The first was a will, drawn in the same eccentric terms as the one which he had returned six months before, to serve as a testament in case of death and as a deed of gift in case of disappearance; but in place of the name of Edward Hyde, the lawyer, with indescribable amazement read the name of Gabriel John Utterson. He looked at Poole, and then back at the paper, and last of all at the dead malefactor stretched upon the carpet.</p>
+    <p>Next they turned to the <code>business</code> table. On the desk, among the <code>neat array</code> of papers, a large envelope was uppermost, and bore, in the doctor’s hand, the name of Mr. Utterson. The lawyer unsealed it, and several enclosures fell to the floor. The first was a will, drawn in the same eccentric terms as the one which he had returned six months before, to serve as a testament in case of death and as a deed of gift in case of disappearance; but in place of the name of Edward Hyde, the lawyer, with indescribable amazement read the name of Gabriel John Utterson. He looked at Poole, and then back at the paper, and last of all at the dead malefactor stretched upon the carpet.</p>
   </Example>
   <Example label="SMALL">
     <small>Next they turned to the business table. on the desk, among the neat array of papers, a large envelope was uppermost, and bore, in the doctor’s hand, the same name of mr.utterson.</small>
