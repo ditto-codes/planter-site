111ed90723e129d36d9ed8d67afdbc6caa26e369
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2023-05-24 20:26:14 -0400

    hash heading component

--- src/components/mdsvex/heading-link.svelte
+++ src/components/mdsvex/heading-link.svelte
@@ -5,24 +5,38 @@
   Add click to copy to clipboard
 -->
 <script>
-  let node;
-
+  import { page } from '$app/stores';
+  
   function linkify(node) {
-    let text = node.firstChild.innerText;
+    let jump = node.querySelector('.jump');
+    let heading = node.querySelector('.heading');
+    let anchor = node.querySelector('a');
+
+    let text = heading.firstChild.innerText;
     let id = text
       .replace(/[\s\_\&\:\;\,\+]+/g, '-')
       .toLowerCase();
 
-    node.href = '#' + id;
-    node.id = id;
+    anchor.href = '#' + id;
+    jump.id = id;
   }
+
+  function copy() {
+    let url = $page.url.href;
+    navigator.clipboard.writeText(url);
+  }
+
 </script>
 
-<!-- svelte-ignore a11y-missing-attribute -->
-<a class="unstyled" bind:this={node} use:linkify>
-  <slot/>
-  <span class="link-hash">#</span>
-</a>
+<div use:linkify>
+  <div class="jump" />
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <div class="heading">
+    <slot/>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <a on:click={copy} class="link-hash">#</a>
+  </div>
+</div>
 
 <style lang="scss">
   // a :global(h1),
@@ -33,7 +47,7 @@
   // a :global(h6) {
   //   margin-bottom: 0;
   // }
-  a {
+  .heading {
     position: relative;
     display: block;
     &:hover {
@@ -43,15 +57,35 @@
     }
   }
   .link-hash {
+    // Reset button styles
+    background: none;
+    border: 0;
+    padding: 0;
+    // Actual styles
     opacity: 0;
     position: absolute;
     left: 0;
     top: 50%;
     transform: translateY(-50%);
-    margin-left: sp(-2);
+    margin-left: sp(-2.25);
     padding: sp(1.5 .5);
     color: $coral;
-    transition: $animate-faster;
+    transition: $animate-fast;
     user-select: none;
+    text-decoration: none;
+    font-size: 2rem;
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+      text-underline-offset: .2em;
+      text-decoration-thickness: 1.5px;
+    }
+  }
+
+  .jump {
+    display: block;
+    position: relative;
+    top: -225px;
+    visibility: hidden;
   }
 </style>
